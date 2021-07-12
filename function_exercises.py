972d64eba9a595cccc50fcbe3c2e409af9678ec7
jamesallen0351 <james.allen0351@gmail.com>
2021-07-12 16:08:31 -0500

    updated working function exercises

--- function_exercises.py
+++ function_exercises.py
@@ -6,31 +6,38 @@ def is_two(a):
         return True
     else:
         False
+
+
 return(is_two(2))
 return(is_two(3))
     
 
-# 2
+# 2 Define a function named is_vowel. It should return True if the passed string is a vowel, False otherwise.
 
-# 3
+# 3 Define a function named is_consonant. It should return True if the passed string is a consonant, False otherwise. Use your is_vowel function to accomplish this.
 
-# 4
+# 4 Define a function that accepts a string that is a word. The function should capitalize the first letter of the word if the word starts with a consonant.
 
-# 5
+# 5 Define a function named calculate_tip. It should accept a tip percentage (a number between 0 and 1) and the bill total, and return the amount to tip. 
 
-# 6
+# 6 Define a function named apply_discount. It should accept a original price, and a discount percentage, and return the price after the discount is applied.
 
-# 7
+# 7 Define a function named handle_commas. It should accept a string that is a number that contains commas in it as input, and return a number as output.
 
-# 8
+# 8 Define a function named get_letter_grade. It should accept a number and return the letter grade associated with that number (A-F).
 
-# 9
+# 9 Define a function named remove_vowels that accepts a string and returns a string with all the vowels removed.
 
-# 10
+# 10 Define a function named normalize_name. It should accept a string and return a valid python identifier, that is:
 
-# 11
+# 11 Write a function named cumulative_sum that accepts a list of numbers and returns a list that is the cumulative sum of the numbers in the list.
 
 # additional exercise
+# Once you've completed the above exercises, follow the directions from https://gist.github.com/zgulde/ec8ed80ad8216905cda83d5645c60886 in order to thouroughly comment your code to explain your code.
+
 
 # bonus exercise
 
+# 1 Create a function named twelveto24. It should accept a string in the format 10:45am or 4:30pm and return a string that is the representation of the time in a 24-hour format. Bonus write a function that does the opposite.
+
+# 2 Create a function named col_index. It should accept a spreadsheet column name, and return the index number of the column.
