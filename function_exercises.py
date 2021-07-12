e3dad9140dced65787fd7b099bfcdb140815ce0b
jamesallen0351 <james.allen0351@gmail.com>
2021-07-12 16:01:49 -0500

    adding new functions exercises

---
+++ function_exercises.py
@@ -0,0 +1,36 @@
+# function_exercises
+
+# 1 Define a function named is_two. It should accept one input and return True if the passed input is either the number or the string 2, False otherwise.
+def is_two(a):
+    if is_two == 2:
+        return True
+    else:
+        False
+return(is_two(2))
+return(is_two(3))
+    
+
+# 2
+
+# 3
+
+# 4
+
+# 5
+
+# 6
+
+# 7
+
+# 8
+
+# 9
+
+# 10
+
+# 11
+
+# additional exercise
+
+# bonus exercise
+
