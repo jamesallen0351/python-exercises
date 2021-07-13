3bda0a151efc36f0a49973557475d5c6ef49da5a
jamesallen0351 <james.allen0351@gmail.com>
2021-07-13 08:26:04 -0500

    working on functions exercises and updated notes

--- function_exercises.py
+++ function_exercises.py
@@ -2,35 +2,59 @@
 
 # 1 Define a function named is_two. It should accept one input and return True if the passed input is either the number or the string 2, False otherwise.
 def is_two(a):
-    if is_two == 2:
+    if a == 2:
         return True
     else:
         False
 
 
-return(is_two(2))
-return(is_two(3))
+print(is_two(2))
+print(is_two(3))
     
 
 # 2 Define a function named is_vowel. It should return True if the passed string is a vowel, False otherwise.
+def is_vowel(a):
+    if a == ["a", "e", "i", "o", "u"]:
+        return True
+    else:
+        False
+
+print(is_vowel(a))
+print(is_vowel(x))
+
 
 # 3 Define a function named is_consonant. It should return True if the passed string is a consonant, False otherwise. Use your is_vowel function to accomplish this.
+def is_consonant()
 
 # 4 Define a function that accepts a string that is a word. The function should capitalize the first letter of the word if the word starts with a consonant.
 
+
 # 5 Define a function named calculate_tip. It should accept a tip percentage (a number between 0 and 1) and the bill total, and return the amount to tip. 
+def calculate_tip()
+
 
 # 6 Define a function named apply_discount. It should accept a original price, and a discount percentage, and return the price after the discount is applied.
+def apply_discount()
 
 # 7 Define a function named handle_commas. It should accept a string that is a number that contains commas in it as input, and return a number as output.
+def handle_commas()
+
 
 # 8 Define a function named get_letter_grade. It should accept a number and return the letter grade associated with that number (A-F).
+def get_letter_grade()
+
 
 # 9 Define a function named remove_vowels that accepts a string and returns a string with all the vowels removed.
+def remove_vowels()
+
 
 # 10 Define a function named normalize_name. It should accept a string and return a valid python identifier, that is:
+def normalize_name()
+
 
 # 11 Write a function named cumulative_sum that accepts a list of numbers and returns a list that is the cumulative sum of the numbers in the list.
+def cumulative_sum()
+
 
 # additional exercise
 # Once you've completed the above exercises, follow the directions from https://gist.github.com/zgulde/ec8ed80ad8216905cda83d5645c60886 in order to thouroughly comment your code to explain your code.
