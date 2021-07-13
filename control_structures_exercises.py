ed873b034de14745f5f0f69aa7ebae8e25615b8a
jamesallen0351 <james.allen0351@gmail.com>
2021-07-13 08:48:38 -0500

    added exercises and notes to control structures

--- control_structures_exercises.py
+++ control_structures_exercises.py
@@ -18,7 +18,17 @@ for weekday in what_day_is_it:
     if day == "Monday" or "Tuesday" or "Wednesday" or "Thursday" or "Friday":
         print("Today is a Weekday")
 else:
-    print("Weekend Vibes")
+    print("Weekend Vibes")  #did not work as intended
+
+# adding review notes
+
+    # Task 1 Control Basics
+    ### A) prompt the user for a day of the week, print out whether the day is Monday or not"
+  day = input(\"What day of the week is it?   \")
+  
+  
+
+
 
 ## c. create variables and make up values for
 
@@ -32,7 +42,7 @@ paycheck = number_of_hours_worked * hourly_rate
 print("This week's paycheck will be", paycheck)
 
 # overtime is time and a half pay
-
+overtime
 
 
 #2 Loop Basics
@@ -110,6 +120,8 @@ for r in range(1, 101):
         print("Buzz")
     else:
         print(r)
+
+
 ### alternate fizzbuzz solution    
 for fizzbuzz in range(1, 100):
     if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
@@ -144,9 +156,39 @@ for fizzbuzz in range(1, 100):
 ## Assume that the user will enter valid integers for the grades.
 ## The application should only continue if the user agrees to.
 ## Grade Ranges:A : 100 - 88, B : 87 - 80, C : 79 - 67, D : 66 - 60, F : 59 - 0
-
 ### bonus Edit your grade ranges to include pluses and minuses (ex: 99-100 = A+)
 
+wants_to_continue = True
+while wants_to_continue:
+    grade = int(input('Input a grade from 0 to 100:\n'))
+    if grade >= 95:
+        print('A+')
+    elif grade >= 90:
+        print('A')
+    elif grade >= 88:
+        print('A-')
+    elif grade >= 85:
+        print('B+')
+    elif grade >= 83:
+        print('B')
+    elif grade >= 80:
+        print('B-')
+    elif grade >= 75:
+        print('C+')
+    elif grade >= 70:
+        print('C')
+    elif grade >= 67:
+        print('C-')    
+    elif grade >= 64:
+        print('D+')
+    elif grade >= 62:
+        print('D')
+    elif grade >= 60:
+        print('D-')
+    else:
+        print('F')
+    if input('Do you want to stop?\n').lower() in ('y', 'yes'):
+        wants_to_continue = False
 
 # 6 Create a list of dictionaries where each dictionary represents a book that you have read. Each dictionary in the list should have the keys title, author, and genre.
 ## Loop through the list and print out information about each book.
